c8724218b347eac477ea788142788d327d0d4db8
Grace Andrizzi <graceandrizzi@graces-mbp.lan>
2018-09-12 17:40:33 -0700

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+var friends = [
+	{
+		name: "Leslie",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/38/Leslie.png/revision/latest?cb=20111015205925",
+		scores: [5, 1, 2, 4, 5, 1, 2, 3, 4, 5]
+	},
+	{
+		name: "Ron",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/5/5c/Ron.jpg/revision/latest?cb=20110809180635",
+		scores: [2, 5, 4, 1, 1, 2, 3, 4, 5, 2]
+	},
+	{
+		name: "Andy",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/d/d1/Andy-0.jpg/revision/latest?cb=20180116071113",
+		scores: [1, 1, 5, 2, 3, 4, 5, 3, 2, 1]
+	},
+	{
+		name: "Ben",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/0/0a/Ben.jpg/revision/latest?cb=20110809181418",
+		scores: [3, 1, 3, 4, 5, 3, 2, 1, 4, 5]
+	},
+	{
+		name: "April",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/e/ed/April-0.png/revision/latest?cb=20180116070637",
+		scores: [3, 1, 4, 3, 4, 5, 1, 2, 4, 5]
+	},
+	{
+		name: "Tom",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/9/9c/Tom.jpg/revision/latest?cb=20110809180711",
+		scores: [3, 1, 4, 5, 2, 1, 2, 3, 4, 5]
+	},
+	{
+		name: "Ann",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/33/Ann_Perkins.jpg/revision/latest?cb=20180116070354",
+		scores: [3, 1, 4, 4, 5, 1, 2, 3, 4, 5]
+	},
+	{
+		name: "Donna",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/5/59/Donna_2.jpg/revision/latest?cb=20111015210204",
+		scores: [3, 1, 4, 5, 1, 2, 4, 4, 4, 2]
+	},
+	{
+		name: "Jerry",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/3/38/Jerry.jpg/revision/latest?cb=20110809181649",
+		scores: [2, 3, 4, 5, 1, 5, 5, 5, 5, 1]
+	},
+	{
+		name: "Chris",
+		photo: "https://vignette.wikia.nocookie.net/parksandrecreation/images/a/a0/ChrisTraeger.jpg/revision/latest?cb=20110809181529",
+		scores: [3, 1, 4, 1, 1, 1, 1, 1, 1, 1]
+	}
+];
+
+module.exports = friends;
